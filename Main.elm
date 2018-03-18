31c029fb0c0e9f4cfedca3a7308a1edc22630f40
tamamu <tamamu.1r1s@gmail.com>
2018-03-19 02:15:08 +0900

    Componentize cells

--- Main.elm
+++ Main.elm
@@ -1,48 +1,68 @@
 module Main exposing (..)
 
-import Editor
-import Html exposing (Html)
+import Cell
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (style)
 
 
 type alias Model =
-    { editor : Editor.Model
-    }
+    { cells : List Cell.Model }
 
 
 model : Model
 model =
-    { editor = Editor.model
+    { cells =
+        [ Cell.model 1
+        , Cell.model 2
+        , Cell.model 3
+        ]
     }
 
 
 type Msg
-    = Editor Editor.Msg
+    = Cell Int Cell.Msg
+
+
+updateCell : Int -> Cell.Msg -> Cell.Model -> ( Cell.Model, Cmd Cell.Msg )
+updateCell id msg model =
+    if model.id == id then
+        Cell.update msg model
+    else
+        model ! []
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Editor msg ->
+        Cell id msg ->
             let
-                ( editor, cmd ) =
-                    Editor.update msg model.editor
+                ( cells, cmds ) =
+                    List.unzip <|
+                        List.map (updateCell id msg) model.cells
             in
-            { model | editor = editor } ! [ Cmd.map Editor cmd ]
+            { model | cells = cells } ! List.map (\cmd -> Cmd.map (Cell id) cmd) cmds
 
 
 view : Model -> Html Msg
 view model =
-    Html.map Editor (Editor.render model.editor)
+    div [] <|
+        List.map (\cell -> Html.map (Cell cell.id) (Cell.render cell)) model.cells
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch <|
+        List.map
+            (\cell ->
+                Sub.map (Cell cell.id) (Cell.subscriptions cell)
+            )
+            model.cells
 
 
 main =
     Html.program
         { view = view
         , update = update
-        , subscriptions =
-            \model ->
-                Sub.batch
-                    [ Sub.map Editor (Editor.subscriptions model.editor)
-                    ]
-        , init = ( model, Editor.init )
+        , subscriptions = \m -> Sub.none
+        , init = ( model, Cmd.none )
         }
